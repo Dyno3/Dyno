606b394e6942a206113aa01d2e061a6c64d08d9d
Dyno3 <dynamogaming36936@gmail.com>
2024-03-12 21:29:05 +0530

    python

---
+++ python.py
@@ -0,0 +1,11 @@
+mark=int(input("enter your mark"))
+if (mark>=90):
+    print("ograde")
+elif (mark>=89):
+    print("Agrade")
+elif (mark>=79):
+    print("B grade")
+elif (mark>=35):
+    print("Cgrade") 
+else:
+    print("fail")           
